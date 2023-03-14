7b2afc80d6a6fdaf9d6f53d8cb2a19d1d8fcc283
Cristian Mendieta <cjmm0811@gmail.com>
2023-03-13 21:12:56 -0500

    parcialBack2

---
+++ parcial-config-server/src/test/java/com/example/parcialconfigserver/ParcialConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
